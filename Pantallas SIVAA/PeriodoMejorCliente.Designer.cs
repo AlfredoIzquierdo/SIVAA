f0d7042670da713cdf21014189460b742292a1d2
Mario Dominguez <marioa0717@gmail.com>
2023-04-22 02:19:32 -0700

    Revert "Formato reporte clientes frecuentes y pequenos detalles"
    This reverts commit 7898d7ec038691e23d02d836bcd2d694827f8bc3.

--- Pantallas SIVAA/PeriodoMejorCliente.Designer.cs
+++ Pantallas SIVAA/PeriodoMejorCliente.Designer.cs
@@ -30,15 +30,7 @@
         {
             button1 = new Button();
             label1 = new Label();
-            numericUpDown1 = new NumericUpDown();
-            numericUpDown2 = new NumericUpDown();
-            numericUpDown3 = new NumericUpDown();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            comboBox1 = new ComboBox();
             SuspendLayout();
             // 
             // button1
@@ -46,7 +38,7 @@
             button1.BackColor = Color.FromArgb(82, 101, 143);
             button1.Font = new Font("Lucida Fax", 9F, FontStyle.Regular, GraphicsUnit.Point);
             button1.ForeColor = SystemColors.Control;
-            button1.Location = new Point(255, 174);
+            button1.Location = new Point(156, 99);
             button1.Name = "button1";
             button1.Size = new Size(178, 49);
             button1.TabIndex = 5;
@@ -58,78 +50,31 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Lucida Fax", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(12, 48);
+            label1.Location = new Point(27, 39);
             label1.Name = "label1";
             label1.Size = new Size(317, 20);
             label1.TabIndex = 4;
             label1.Text = "Selecciona el periodo que abarcara:";
             // 
-            // numericUpDown1
+            // comboBox1
             // 
-            numericUpDown1.Location = new Point(18, 136);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(49, 27);
-            numericUpDown1.TabIndex = 6;
-            // 
-            // numericUpDown2
-            // 
-            numericUpDown2.Location = new Point(108, 136);
-            numericUpDown2.Name = "numericUpDown2";
-            numericUpDown2.Size = new Size(49, 27);
-            numericUpDown2.TabIndex = 7;
-            // 
-            // numericUpDown3
-            // 
-            numericUpDown3.Location = new Point(196, 136);
-            numericUpDown3.Name = "numericUpDown3";
-            numericUpDown3.Size = new Size(49, 27);
-            numericUpDown3.TabIndex = 8;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(18, 102);
-            label2.Name = "label2";
-            label2.Size = new Size(35, 20);
-            label2.TabIndex = 9;
-            label2.Text = "Dia:";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(108, 102);
-            label3.Name = "label3";
-            label3.Size = new Size(39, 20);
-            label3.TabIndex = 10;
-            label3.Text = "Mes:";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(196, 102);
-            label4.Name = "label4";
-            label4.Size = new Size(39, 20);
-            label4.TabIndex = 11;
-            label4.Text = "Año:";
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Dia", "Semana", "Mes ", "Anual" });
+            comboBox1.Location = new Point(350, 36);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 3;
             // 
             // PeriodoMejorCliente
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(646, 254);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(numericUpDown3);
-            Controls.Add(numericUpDown2);
-            Controls.Add(numericUpDown1);
+            ClientSize = new Size(528, 185);
             Controls.Add(button1);
             Controls.Add(label1);
+            Controls.Add(comboBox1);
             Name = "PeriodoMejorCliente";
             Text = "PeriodoMejorCliente";
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -138,11 +83,6 @@
 
         private Button button1;
         private Label label1;
-        private NumericUpDown numericUpDown1;
-        private NumericUpDown numericUpDown2;
-        private NumericUpDown numericUpDown3;
-        private Label label2;
-        private Label label3;
-        private Label label4;
+        private ComboBox comboBox1;
     }
 }
